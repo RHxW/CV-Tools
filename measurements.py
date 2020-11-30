e9fa0a177aa771c1e458ed922f29d1549661b084
wangyunhang <38681688+RHxW@users.noreply.github.com>
2020-11-30 16:35:51 +0800

    update file
    Signed-off-by: wangyunhang <38681688+RHxW@users.noreply.github.com>

---
+++ measurements.py
@@ -0,0 +1,52 @@
+def get_measurements(TP, FN, FP, TN, alpha:int=None):
+    P = TP + FN
+    N = FP + TN
+        if P == 0 or N == 0:
+        return {
+            "TP": TP,
+            "FN": FN,
+            "FP": FP,
+            "TN": TN
+        }
+    P_ = TP + FP
+    N_ = FN + TN
+    # Accuracy
+    acc = (TP + TN) / (TP + TN + FP + FN)
+
+    # Error rate (err = 1 - acc)
+    err = (FP + FN) / (TP + TN + FP + FN)
+
+    # sensitive
+    sens = TP / P
+
+    # specificity
+    spec = TN / N
+
+    # Precision
+    prec = TP / (TP + FP)
+
+    # recall (==sensitive)
+    recall = TP / (TP + FN)
+
+    # F-measure
+    # F1
+    F1 = (2 * prec * recall) / (prec + recall)
+
+    # F-alpha
+    if alpha is None or type(alpha) is not int:
+        Fa = None
+    else:
+        Fa = ((alpha ** 2 + 1) * prec * recall) / (alpha ** 2 * (prec + recall))
+
+    ret = {
+        "accuracy": acc,
+        "error_rate": err,
+        # "sensitive": sens,
+        "specificity": spec,
+        "precision": prec,
+        "recall": recall,
+        "F1": F1,
+        "Fa": Fa,
+        "alpha": alpha
+    }
+    return ret
