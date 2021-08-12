3c385f92945021f45edb38c3675f0e18ca6c7cf9
wangyunhang <38681688+RHxW@users.noreply.github.com>
2021-08-12 08:59:33 +0800

    update file

--- blur.py
+++ blur.py
@@ -58,3 +58,30 @@ def lowLevelsize(img, level=None):
     fsized = cv2.resize(dsized, (w, h))
     return fsized
 
+##马赛克
+def do_mosaic(frame, x, y, w, h, neighbor=9):
+    """
+    马赛克的实现原理是把图像上某个像素点一定范围邻域内的所有点用邻域内左上像素点的颜色代替，这样可以模糊细节，但是可以保留大体的轮廓。
+    :param frame: opencv frame
+    :param int x :  马赛克左顶点
+    :param int y:  马赛克右顶点
+    :param int w:  马赛克宽
+    :param int h:  马赛克高
+    :param int neighbor:  马赛克每一块的宽
+    """
+    fh, fw = frame.shape[0], frame.shape[1]
+    if (y + h > fh) or (x + w > fw):
+        return
+    for i in range(0, h - neighbor, neighbor):  # 关键点0 减去neighbour 防止溢出
+        for j in range(0, w - neighbor, neighbor):
+            rect = [j + x, i + y, neighbor, neighbor]
+            color = frame[i + y][j + x].tolist()  # 关键点1 tolist
+            left_up = (rect[0], rect[1])
+            right_down = (rect[0] + neighbor - 1, rect[1] + neighbor - 1)  # 关键点2 减去一个像素
+            cv2.rectangle(frame, left_up, right_down, color, -1)
+    return frame
+
+def mosaic_full(img, neighbor):
+    x, y = 0, 0
+    h, w, c = img.shape
+    return do_mosaic(img, x, y, w, h, neighbor)
