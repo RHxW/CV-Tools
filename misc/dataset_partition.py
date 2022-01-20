7c992d291a999b56dbeb7125f4793764c3cdc9b7
wyhn <wang_yunhang@outlook.com>
2022-01-20 10:30:16 +0800

    update file

--- misc/dataset_partition.py
+++ misc/dataset_partition.py
@@ -25,3 +25,43 @@ def train_test_dataset_partition(trainset_ratio: float, dataset_all: list, remai
     train_set = dataset[:train_n]
     test_set = dataset[train_n:]
     return train_set, test_set
+
+
+def dataset_partition(ratios: list, dataset_origin: list, inplace: bool = False):
+    """
+    数据集按任意比例划分
+    :param ratios:
+    :param dataset_origin:
+    :param inplace:
+    :return:
+    """
+    total_ratio = sum(ratios)
+    if total_ratio < 1:
+        ratios.append((1 - total_ratio))
+    elif total_ratio > 1:
+        raise RuntimeError('total ratio greater than 1 !!!')
+
+    assert isinstance(dataset_origin, list)
+
+    n = len(dataset_origin)
+    counts = []
+    for r in ratios[:-1]:
+        counts.append(int(n * r))
+    counts.append(n - sum(counts))
+
+    if inplace:
+        dataset = dataset_origin
+    else:
+        dataset = copy.deepcopy(dataset_origin)
+    random.shuffle(dataset)
+
+    partitions = []
+
+    start_idx = 0
+    for c in counts[:-1]:
+        partitions.append(dataset[start_idx:start_idx + c])
+        start_idx += c
+
+    partitions.append(dataset[start_idx:])
+
+    return partitions
